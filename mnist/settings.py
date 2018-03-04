a92b52ea71597df29f5193ae6b7d72f29a701ba0
paulbellmann <paul.bellmann@yahoo.de>
2018-03-04 12:13:48 +0100

    static bug

--- mnist/settings.py
+++ mnist/settings.py
@@ -121,11 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
+    
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
